a9b2a82164936cc5c959e3c417c240e1eea55d71
Kashif-79 <kashif.r.reza@gmail.com>
2025-10-12 13:57:07 +0600

    Deploy hanling

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,11 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  base: "/", // 👈 ensures correct asset paths
+  build: {
+    outDir: "dist", // 👈 matches what you told Vercel
+  },
+});
